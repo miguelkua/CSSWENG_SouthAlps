4c92ad66a3934eb212b1099e0eb53274b2e3b2ed
DigiDude21 <104496357+DigiDude21@users.noreply.github.com>
2023-10-22 16:07:23 +0800

    Back-End Patch
    I made copies of the html files we have as handlebars, and placed them in views
    
    Added a javascript folder as well for other back-end stuff
    
    We porbbaly need to update/merge all the npm installs we have together

---
+++ javascript/adminhotbar.js
@@ -0,0 +1,101 @@
+let isEditText = false;
+let isEditImage = false;
+
+const editTextButton = document.getElementById('edit-text');
+const editImgButton = document.getElementById('edit-img');
+// const addButton = document.getElementById('add');
+// const deleteButton = document.getElementById('delete');
+
+document.addEventListener('DOMContentLoaded', function () {
+    editTextButton.addEventListener('click', function () {
+        // alert('Edit text button clicked');
+        toggleMode('edit-text');
+    });
+
+    editImgButton.addEventListener('click', function () {
+        // alert('Edit text image clicked');
+        toggleMode('edit-img');
+    });
+
+    // addButton.addEventListener('click', function () {
+    //     alert('Add button clicked');
+    // });
+
+    // deleteButton.addEventListener('click', function () {
+    //     alert('Delete button clicked');
+    // });
+});
+
+function toggleMode(action){
+    const textEntries = document.querySelectorAll('p'); //perhaps add more elements onto this to change them
+    const imageEntries = document.querySelectorAll('img');
+
+    if(action === 'edit-text'){
+        //remove the others
+        isEditImage = false;
+        editImgButton.classList.remove('active');
+        imageEntries.forEach((entry) => {
+            entry.removeEventListener('click', selectEntry);
+        });
+
+        // if we click it again
+        if(editTextButton.classList.contains('active')){
+            editTextButton.classList.remove('active');
+            isEditText = false;
+
+            //what removes the ability
+            textEntries.forEach((entry) => {
+                entry.removeEventListener('click', selectEntry);
+            });
+        }
+
+        else{
+            isEditText = true;
+            editTextButton.classList.add('active');
+
+            //what allows to select text stuff
+            textEntries.forEach((entry) => {
+                entry.addEventListener('click', selectEntry);
+            });
+        }
+    }
+
+    else if(action === 'edit-img'){ 
+        isEditText = false;
+        editTextButton.classList.remove('active');
+        textEntries.forEach((entry) => {
+            entry.removeEventListener('click', selectEntry);
+        });
+
+        if(editImgButton.classList.contains('active')){
+            editImgButton.classList.remove('active');
+            isEditImage = false;
+
+            imageEntries.forEach((entry) => {
+                entry.removeEventListener('click', selectEntry);
+            });
+        }
+
+        else{
+            isEditImage = true;
+            editImgButton.classList.add('active');
+
+            imageEntries.forEach((entry) => {
+                entry.addEventListener('click', selectEntry);
+            });
+        }
+    }
+
+}
+
+function selectEntry(event) {
+    const clickedElement = event.target;
+
+    if(isEditText == true){
+        alert(clickedElement.textContent);
+    }
+
+    else if(isEditImage == true){
+        alert(clickedElement.alt)
+    }
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// View engine setup
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views')); // views is the folder name
+
+//enables the use of the assets folder
+app.use('/assets', express.static('assets'));
+app.use('/assets/icons', express.static('icons'));
+app.use('/assets/logo', express.static('logo'));
+
+//enables the use of the css folder
+app.use('/css', express.static('css'));
+
+//enables the use of the javascript folder
+app.use('/javascript', express.static('javascript'));
+
+// Defaults to home in the views folder
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+// partial code
+let isAdminMode = true;
+
+// the rest of the pages interacting with each other
+app.get('/home', (req, res) => {
+    res.render('home', {layout: false, isAdminMode});
+});
+
+app.get('/facility', (req, res) => {
+    res.render('facility', {layout: false, isAdminMode});
+});
+
+app.get('/getQuote', (req, res) => {
+    res.render('getQuote', {layout: false, isAdminMode});
+});
+
+app.get('/index', (req, res) => {
+    res.render('index', {layout: false, isAdminMode});
+});
+
+app.get('/services', (req, res) => {
+    res.render('services', {layout: false, isAdminMode});
+});
+
+app.get('/careers', (req, res) => {
+    res.render('careers', {layout: false, isAdminMode});
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {layout: false, isAdminMode});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

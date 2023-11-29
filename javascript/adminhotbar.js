1fc5817ac0ee1ed07804773e74fc84fcfe3c28ed
DigiDude21 <104496357+DigiDude21@users.noreply.github.com>
2023-11-30 01:10:27 +0800

    small test

--- javascript/adminhotbar.js
+++ javascript/adminhotbar.js
@@ -647,6 +647,7 @@ function selectEntry(event) {
 
             else if(classes.includes('careerName') == true){
                 isCareerName = true;
+                alert("Test")
             }
 
             else if(classes.includes('facilityDesc') == true){
